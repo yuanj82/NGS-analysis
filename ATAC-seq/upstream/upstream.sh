ca5edce8730791ff6ff4926e35dc0ba4ba1aeea6
yuanj <yuanj.top@gmail.com>
2024-03-28 18:56:54 +0800

    更新atac-seq环境与部分代码

--- ATAC-seq/upstream/upstream.sh
+++ ATAC-seq/upstream/upstream.sh
@@ -1,3 +1,18 @@
+
+nohup prefetch -O . $(<SRR_Acc_List.txt) &
+
+nohup ls *.fastq.gz | xargs fastqc -t 2 -O ../fastqc_reports &
+
+
+for i in {57..62}
+do
+    trim_galore -q 20 --length 36 --max_n 3 --stringency 3 --fastqc --paired -o SRR58746{i}_1_trim.fastq.gz SRR58746{i}_2_trim.fastq.gz SRR58746{i}_1.fastq.gz SRR58746{i}_2.fastq.gz > {log} 2>&1 
+done
+
+
+
+nohup wget http://ftp.ebi.ac.uk/ensemblgenomes/pub/release-51/plants/fasta/arabidopsis_thaliana/dna/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa.gz &
+
 #!/bin/bash
 for i in *.bam
 do 
