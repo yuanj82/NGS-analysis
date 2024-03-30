fdb0408ecf914b1f2fcb5a964ae2c7790ec4b196
yuanj <yuanj.top@gmail.com>
2024-03-30 15:06:37 +0800

    修改结构

--- RNA-seq/upstream/upstream.sh
+++ RNA-seq/upstream/upstream.sh
@@ -18,31 +18,26 @@ mkdir sra fastq_gz fastqc_reports clean sorted genome_index compared
 
 ############# data preparation #############
 
-cd sra
-
 nohup prefetch -O . $(<SRR_Acc_List.txt) &
 wait # 等待prefetch完成
 
-cat SRR_Acc_List.txt | while read id; do
-    mv -f "${id}/${id}.sra" ./
+cat ./SRR_Acc_List.txt | while read id; do
+    mv -f "${id}/${id}.sra" ./sra
     rm -rf "${id}"
 done
 
 ############# sra to fastq #############
 
-ls *.sra | parallel fastq-dump --gzip --split-3 {} --outdir ../fastqgz
+ls *.sra | parallel fastq-dump --gzip --split-3 {} --outdir ./fastqgz
 
 ############# FastQc quality control #############
 
-cd ../fastq_gz
-ls *.fastq.gz | xargs fastqc -t 2 -O ../fastqc_reports
-cd ../fastqc_reports && multiqc ./  # 合并质量检测报告
+ls ./fastqgz/*.fastq.gz | xargs fastqc -t 2 -O ./fastqc_reports
+multiqc ./fastqc_report  # 合并质量检测报告
 
 ############# Trim_galore filtering sequence #############
 
-cd ../fastq_gz
-
-cat ../SRR_Acc_List.txt | while read id; do 
+cat ./SRR_Acc_List.txt | while read id; do 
     nohup trim_galore \
     -q 20 \
     --length 36 \
@@ -50,49 +45,44 @@ cat ../SRR_Acc_List.txt | while read id; do
     --stringency 3 \
     --fastqc \
     --paired \
-    -o ../clean/ \
-    ${id}_1.fastq.gz ${id}_2.fastq.gz &
+    -o ./clean/ \
+    ./fastqgz/${id}_1.fastq.gz ./fastqgz/${id}_2.fastq.gz &
 done
 wait # 等待trim_galore完成
 
 ############# Reply to reference genome #############
 
-genome='../oryza_sativa.fa'
-species='oryza_sativa'
-cd ../genome_index
+genome='../oryza_sativa.fa'   # 基因组文件路径（需提前下载解压）
+species='oryza_sativa'  # 物种名称（作为索引名称）
+cd ./genome_index
+cp ${genome} ./
 
-nohup hisat2-build -p 4 ${genome} ${species} &
-wait # 等待hisat2-build完成
+hisat2-build -p 4 ${species}.fa ${species}
+cd ..
 
-cat ../SRR_Acc_List.txt | while read id;
+cat ./SRR_Acc_List.txt | while read id;
 do
     hisat2 \
-    -x ${species}/${species} \
+    -x ./genome_index/${species} \
     -p 5 \
-    -1 ${id}_1.fastq.gz \
-    -2 ${id}_2.fastq.gz \
-    -S ${id}.sam
+    -1 ./clean/${id}_1.fastq.gz \
+    -2 ./clean/${id}_2.fastq.gz \
+    -S ./compared/${id}.sam
 done
-
-mv *.sam ../compared
 
 ############# sam to bam #############
 
-cd ../compared
-cat ../SRR_Acc_List.txt | while read id ;do
+cat ./SRR_Acc_List.txt | while read id ;do
     samtools \
     sort \
     -n -@ 5 \
-    ${id}.sam \
-    -o ${id}.bam
+    ./compared/${id}.sam \
+    -o ./sorted/${id}.bam
 done
-
-rm *.sam
-mv ${id}.bam ../sorted
 
 ############# featureCounts #####
 
-cd ../sorted
+cd ./sorted
 bam=$(ls *)
 gtf='../oryza_sativa.gff3'
 featureCounts \
